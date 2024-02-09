8051448e53c60b4bc0236b8e0baf46138057175c
Alexey Volkov <vollkovalex1991@mail.ru>
2024-02-09 20:33:39 +0400

    Fix bugs

--- src/api/routers.py
+++ src/api/routers.py
@@ -5,7 +5,7 @@ from .service import PatientManager
 router = APIRouter()
 
 
-@router.post("/create_patient_record", response_model=schemas.PatientCreateDB)
+@router.post("/create_patient_record", response_model=schemas.Patient)
 async def create_patient_record(
 	access_token: str,
   	patient_data: schemas.PatientCreate
@@ -20,8 +20,8 @@ async def create_patient_record(
 async def get_patient_records(
     access_token: str,
     patient_id: int,
-    limit: int = 0,
-    offset: int = 100,
+    limit: int = 100,
+    offset: int = 0,
 ):
     patient_manager = PatientManager()
     patient_crud = patient_manager.patient_crud
@@ -41,15 +41,16 @@ async def get_all_patient_records(
 
     return await patient_crud.get_all_patient_records(access_token=access_token, limit=limit, offset=offset)
 
-@router.patch("/update_patient_record", response_model=schemas.PatientUpdate)
+@router.patch("/update_patient_record", response_model=schemas.Patient)
 async def update_patient_record(
     access_token: str, 
-    pathient_data: schemas.PatientUpdate
+    patient_id: int, 
+    patient_data: schemas.PatientUpdate
 ):
     patient_manager = PatientManager()
     patient_crud = patient_manager.patient_crud
 
-    return await patient_crud.update_patient_record(access_token=access_token, patient_in=pathient_data)
+    return await patient_crud.update_patient_record(access_token=access_token, patient_in=patient_data, patient_id=patient_id)
 
 @router.delete("/delete_patient_record")
 async def delete_patient_record(
